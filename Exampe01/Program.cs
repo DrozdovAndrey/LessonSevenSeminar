005344ce9b168f5754a39c7121f780768b95c4a4
Andrey Drozdov <drozdov_andrey@list.ru>
2022-07-14 21:26:00 +0300

    collection

---
+++ Exampe01/Program.cs
@@ -0,0 +1,29 @@
+﻿List<int> list = new List<int>();
+
+
+FillList(list, 5);
+
+void FillList(List<int> list, int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        list.Add(new Random().Next(0,11));
+    }
+}
+
+foreach (int number in list)
+{
+    Console.WriteLine(number);
+}
+
+
+Console.WriteLine();
+
+List<int> evenNumbers = list.FindAll(x => x % 2 == 0);
+
+
+foreach (int x in evenNumbers)
+{
+    Console.WriteLine(x);
+}
+

1ed33c52987c04053f29856c10813092fcba347d
Andrey Drozdov <drozdov_andrey@list.ru>
2022-07-14 22:59:29 +0300

    task2 done

---
+++ Task2/Program.cs
@@ -0,0 +1,51 @@
+﻿// 1. Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n.
+//  Выведите полученный массив на экран.
+
+// m = 3, n = 4.
+
+// 0 1 2 3
+
+// 1 2 3 4
+
+// 2 3 4 5
+
+
+namespace Seminar7
+{
+    class Task2
+    {
+        static void Main(string[] args)
+        {
+            int m = new Random().Next(1, 11);
+            int n = new Random().Next(1, 11);
+            Console.WriteLine($"m = {m}, n = {n}");
+            int[,] array = new int[m, n];
+            FillArray(array);
+            PrintArray(array);
+        }
+
+        static void FillArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i,j] = i+j;
+                }
+            }
+        }
+
+        static void PrintArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        
+    }
+}
